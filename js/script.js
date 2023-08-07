8d9ec8a89e030e8cb20140e7f13d6022064176f2
mzhirnov <maximz2009@gmail.com>
2023-08-07 21:14:23 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    const button = document.getElementById('toggleStyle');
+
+    button.addEventListener('click', function() {
+        const body = document.body;
+
+        if (body.classList.contains('dev')) {
+            body.classList.remove('dev');
+            body.classList.add('normal');
+            button.textContent = "DOS theme"
+        } else {
+            body.classList.remove('normal');
+            body.classList.add('dev');
+            button.textContent = "Bright theme"
+        }
+    });
+}
